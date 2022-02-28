fc2c75171da6dd2c6576959939169245d069a19b
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-03-01 05:32:37 +0600

    no result mssg added

--- js/application .js
+++ js/application .js
@@ -1,8 +1,9 @@
 const searchText = document.getElementById('search-field');
 const spinner = document.getElementById('spinner');
+const content = document.getElementById('content');
 
 
-// fetching data from url and pass to the calling function
+// fetching data from url and pass to the calling function; url = api_url  && fn = function name, which is declare to recieve the fetching data for nxt operation;
 const getData = (url,fn) => {
     loading('flex');
     fetch(url)
@@ -11,16 +12,31 @@ const getData = (url,fn) => {
         if(data.status && searchText.value!=''){
             fn(data);
         }else{
-            console.log('no data found');
             loading('none');
+            noResult();
         }
     })
 }
 
-// spinners show and hiding function
+// spinners show and hiding function with clearing the body of content
 const loading = (dProperty) =>{
+    content.textContent='';
     spinner.style.display = dProperty;
 }
+
+// no result mssg
+const noResult = () => {
+    const mssg = document.createElement('div');
+    mssg.innerHTML=`
+        <div class="card border-0 my-3">
+            <div class="card-body text-danger d-inline-block mx-auto border border-danger rounded-3">
+                <h5 class="card-title text-center">No Result Found !!</h5>
+            </div>
+        </div>
+    `
+    content.appendChild(mssg);
+}
+
 
 const loadData = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText.value}`;
